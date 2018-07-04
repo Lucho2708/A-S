c29c977bd0956d3b700e674c34927957f40d887e
fabiansuarez <38958325+fabiansuarez@users.noreply.github.com>
2018-07-04 00:05:05 -0500

    actualizacion
    incorporacion de paginacion en cliente  con search

--- resources/views/cliente/listar.blade.php
+++ resources/views/cliente/listar.blade.php
@@ -7,12 +7,79 @@
 
 @section('contenido')
 
+ 
+      
+      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      
+      <link rel="stylesheet" type="text/css" href="css/table.css">
+           
 
 
+
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="price-box">VER Clientes Tables</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active ">VER PROVEEDORES Tables</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+<div class="container-fluid">
+        <!-- Basic Examples -->
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div >
+                        
+
+                    </div>
+
+
+
+                     <div class="card-body">
+    
+    
+    
+                
+       <div class="row">
+      
+           <div class="col-sm-12 col-md-6">
+   
+
+    <div class="form-group">  <!--    Show Numbers Of Rows    -->
+          <br>
+          <select class  ="" name="state" id="maxRows">
+             <option value="2000">Show ALL Rows</option>
+             <option value="5">5</option>
+             <option value="10">10</option>
+             <option value="15">15</option>
+             <option value="20">20</option>
+             <option value="50">50</option>
+             <option value="70">70</option>
+             <option value="100">100</option>
+            </select>
+          
+          </div></div>
+
+          
+
+      <div class="col-sm-12 col-md-6">
     
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <label>Busqueda:<input id="myInput" type="text" placeholder="Search.."></label></div></div>
 
+
+ 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script> /*  este estilo es para la  busqueda*/
 $(document).ready(function(){
@@ -26,69 +93,42 @@ $(document).ready(function(){
 </script>
 
 
-<section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="price-box">VER CLIENTE </h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active ">VER CLIENTE </li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+<style>  /*  este estilo es para la tabla*/
+#customers {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#customers td, #customers th {
+    border: 1px solid #ddd;  /*  para el color de la linea tabla*/
+    padding: 8px;
+    text-align: center;
+}
+
+#customers tr:nth-child(even){background-color: none;}   /*  para el color de la columnas*/
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: center;
+    background-color: #4CAF50;
+    color: white;
+
+}
+</style>
+
+   
+      
 
 
-<div class="container-fluid">
-        <!-- Basic Examples -->
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        
+<div class="table-responsive" id="customers">
 
-                    </div>
-
-
-
-     
-        <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Data Table With Full Features</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-
-              <div id="example1_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
-                <div class="row">
-                  <div class="col-sm-12 col-md-6">
-                    <div class="dataTables_length" id="example1_length">
-                   
-                    <div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div></div>
-
-                  <div class="col-sm-12 col-md-6"><div id="example1_filter"  class="dataTables_filter">
-
-
-                   <label>Search:<input id="myInput" type="text" placeholder="Search.."></label></div></div></div>
-
-                  
-
-
-                      <div class="row">
-
-                  <div class="table-responsive">
-
-                        <div class="col-sm-12">
-
-                      <table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
-                <thead>
-                <tr role="row">
-                  <tr>
-                  <th>ID</th>
+<table class="table table-striped table-class" id= "table-id" >
+  <tr>
+                    <th>ID</th>
                     <th>Nombres completos</th>
                     <th>Apellidos</th>
                     <th>Tipo de identidad</th>
@@ -101,14 +141,8 @@ $(document).ready(function(){
                     <th>Dirección</th>
                     <th>Tipo de persona</th>
                     <th>Profesión</th>
-                  
-
-
-
-                  </tr>
-                </thead>
-
-            <tbody id="myTable">
+  </tr>
+   <tbody id="myTable">
                 <tr>
                 @foreach($cliente as $cliente)
 
@@ -129,94 +163,47 @@ $(document).ready(function(){
                @endforeach
     
   </tbody>
-</table>
-                
-                
-                
-                       
-                
-                
-                
-                
-                
-                
-                
-        
-      <div class="col-sm-12 col-md-5">
-        
-        <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 21 to 30 of 57 entries</div></div>
-        
 
-         <!-- ## Paginacion de next -->
-         <div class="col-sm-12 col-md-15">
 
-        <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+  </table>
 
-        <ul class="pagination">
 
-        <li class="paginate_button page-item previous disabled" id="example1_previous">
+<!--    Start Pagination -->
+      <div class='pagination-container' >
+        <nav>
+          <ul class="pagination">
+           <!-- Here the JS Function Will Add the Rows -->
+          </ul>
+        </nav>
+      </div>
 
-        <a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+</div> <!--     End of Container -->
 
-        <li class="paginate_button page-item active">
+      <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+      <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/js.js"></script>
+        <script type="text/javascript">
+          getPagination('#table-id');
+          //getPagination('.table-class');
+          //getPagination('table');
 
-        <a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
 
-        <li class="paginate_button page-item ">
-        <a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+          
+        </script><script type="text/javascript">
 
-        <li class="paginate_button page-item ">
-        <a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
 
-        <li class="paginate_button page-item ">
-        <a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
-        <li class="paginate_button page-item ">
-        <a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
-
-        <li class="paginate_button page-item ">
-        <a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
-
-        <li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul>
-    </div></div>
-
-    </div>
-
-      
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables -->
-<script src="plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="plugins/datatables/dataTables.bootstrap4.min.js"></script>
-<!-- SlimScroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-<!-- page script -->
-<script>
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false
-    });
-  });
 </script>
-      
 
-                        
+</div></div></div></div></div>
 
-        <!-- #END# Basic Examples -->
-
-
-    </div>
 @endsection
--- resources/views/garantias/listar.blade.php
+++ resources/views/garantias/listar.blade.php
@@ -24,49 +24,113 @@
                     </div>
 
     
-  <!-- Basic Examples -->
-        
-                        <div class="body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                     
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+<script> /*  este estilo es para la  busqueda*/
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+
+
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="price-box">VER CLIENTE </h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active ">VER CLIENTE </li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+<div class="container-fluid">
+        <!-- Basic Examples -->
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        
+
                     </div>
-                </div>
+
+
+
+     
+        <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Data Table With Full Features</h3>
             </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+
+              <div id="example1_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <div class="dataTables_length" id="example1_length">
+                   
+                    <div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div></div>
+
+                  <div class="col-sm-12 col-md-6"><div id="example1_filter"  class="dataTables_filter">
+
+
+                   <label>Search:<input id="myInput" type="text" placeholder="Search.."></label></div></div></div>
+
+                  
+
+
+                      <div class="row">
+
+                  <div class="table-responsive">
+
+                        <div class="col-sm-12">
+
+                      <table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                <thead>
+                <tr role="row">
+                  <tr>
+                  <th>ID</th>
+                    <th>fecha_inicio</th>
+                    <th>fecha_final</th>
+                    <th>descripcion</th>
+                    <th>daños</th>
+                    
+
+
+
+                  </tr>
+                </thead>
+
+            <tbody id="myTable">
+                <tr>
+                @foreach($cliente1 as $cliente1)
+
+         <td>{{$cliente1->id}}</td>
+         <td>{{$cliente1->fecha_inicio}}</td>
+         <td>{{$cliente1->fecha_final}}</td> 
+         <td>{{$cliente1->descripcion}}</td>
+         <td>{{$cliente1->daños}}</td>
+         
+            </tr>
+               @endforeach
+    
+  </tbody>
+</table>
+
+
 
     
 @endsection
