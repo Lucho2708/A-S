e866e163ba621d60aae7e64728b1acb509c7b316
fabiansuarez <38958325+fabiansuarez@users.noreply.github.com>
2018-07-04 16:52:00 -0500

    cambios checkbox
    cambio a listar garantia

--- resources/views/garantias/listar.blade.php
+++ resources/views/garantias/listar.blade.php
@@ -6,9 +6,23 @@
 @section('styles')
 <!-- Tables style -->
   <link rel="stylesheet" href="{{asset("plugins/datatables/dataTables.bootstrap4.css")}}">
+ 
 @endsection
 
 @section('contenido')
+
+
+
+<p><input type="checkbox" name="fecha_inicio" checked="checked" > Fecha Inicio
+  <input type="checkbox" name="fecha_final" checked="checked" > Fecha Final
+  <input type="checkbox" name="descripcion"checked="checked" > Descripción
+ <input type="checkbox" name="daños" checked="checked"> Daños </p>
+
+ 
+
+
+
+ <link rel="stylesheet" href="css/styles.css">
     <!-- Main content -->
     <section class="content">
       <div class="row">
@@ -17,36 +31,44 @@
             <div class="card-header">
               <h3 class="card-title">Data Table With Full Features</h3>
             </div>
+
             <!-- /.card-header -->
             <div class="card-body">
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                 <tr>
-                  <th>Fecha Inicio</th>
-                  <th>Fecha Final</th>
-                  <th>Descripción</th>
-                  <th>Daños</th>
+                  <th class="fecha_inicio" style="display: table-cell;">Fecha Inicio</th>
+                  <th class="fecha_final" style="display: table-cell;">Fecha Final</th>
+                  <th class="descripcion" style="display: table-cell;">Descripción</th>
+                  <th class="daños" style="display: table-cell;">Daños</th>
                 </tr>
                 </thead>
-                <tbody>
+               
                 <tr>
                   @foreach($cliente1 as $cliente1)
-                   <td>{{$cliente1->fecha_inicio}}</td>
-                   <td>{{$cliente1->fecha_final}}</td> 
-                   <td>{{$cliente1->descripcion}}</td>
-                   <td>{{$cliente1->daños}}</td>
+                    <td class="fecha_inicio" style="display: table-cell;">{{$cliente1->fecha_inicio}}</td>
+                   <td class="fecha_final" style="display: table-cell;">{{$cliente1->fecha_final}}</td> 
+                   <td class="descripcion" style="display: table-cell;">{{$cliente1->descripcion}}</td>
+                   <td class="daños" style="display: table-cell;">{{$cliente1->daños}}</td>
                 </tr>
                   @endforeach
                 </tbody>
-                <tfoot>
-                <tr>
-                  <th>Fecha Inicio</th>
-                  <th>Fecha Final</th>
-                  <th>Descripción</th>
-                  <th>Daños</th>
-                </tr>
-                </tfoot>
+              <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>$("input:checkbox:not(:checked)").each(function() {
+    var column = "table ." + $(this).attr("name");
+    $(column).hide();
+});
+
+$("input:checkbox").click(function(){
+    var column = "table ." + $(this).attr("name");
+    $(column).toggle();
+});
+//# sourceURL=pen.js
+</script>
               </table>
+
+
+
             </div>
             <!-- /.card-body -->
           </div>
@@ -77,4 +99,6 @@
     });
   });
 </script>
+
 @endsection
+
